a1b0deb321ecfc02923749660e705845e523061e
iamnevir <hiromansofaoi@gmail.com>
2023-11-02 23:44:11 +0700

    upload

---
+++ src/hoc/SectionWrapper.tsx
@@ -0,0 +1,34 @@
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { staggerContainer } from "../utils/motion";
+
+const StarWrapper = ({
+  Component,
+  idName,
+}: {
+  Component: () => JSX.Element;
+  idName: string;
+}) =>
+  function HOC() {
+    return (
+      <motion.section
+        variants={staggerContainer({
+          staggerChildren: 0.5,
+          delayChildren: 0.5,
+        })}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+      >
+        <span className="hash-span" id={idName}>
+          &nbsp;
+        </span>
+
+        <Component />
+      </motion.section>
+    );
+  };
+
+export default StarWrapper;
